8a66c40ae36388cfdd7c38f3f12703cb0d8c8998
Dmitry <radiator211@mail.ru>
2021-12-09 03:13:01 +0700

    Work with view controller

--- HomeWork Svetofor/ViewController.swift
+++ HomeWork Svetofor/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSignal: UIView!
+    @IBOutlet weak var yellowSignal: UIView!
+    @IBOutlet weak var greenSignal: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignal.alpha = 0.3
+        yellowSignal.alpha = 0.3
+        greenSignal.alpha = 0.3
+        
     }
 
-
+    @IBAction func startPressed(_ sender: Any) {
+    }
+    
 }
 
