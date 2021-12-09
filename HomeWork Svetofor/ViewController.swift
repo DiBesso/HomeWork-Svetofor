9cd18465830a15ddfc71bf6a898d8cc1657e8c89
Dmitry <radiator211@mail.ru>
2021-12-10 00:52:08 +0700

    Work with logical

--- HomeWork Svetofor/ViewController.swift
+++ HomeWork Svetofor/ViewController.swift
@@ -7,22 +7,50 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var redSignal: UIView!
     @IBOutlet weak var yellowSignal: UIView!
     @IBOutlet weak var greenSignal: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        redSignal.alpha = 0.3
-        yellowSignal.alpha = 0.3
-        greenSignal.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 10
+        
+        redSignal.alpha = lightIsOff
+        yellowSignal.alpha = lightIsOff
+        greenSignal.alpha = lightIsOff
         
     }
 
-    @IBAction func startPressed(_ sender: Any) {
+    @IBAction func startPressed() {
+        
+        switch currentLight {
+        case .red:
+            greenSignal.alpha = lightIsOff
+            redSignal.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redSignal.alpha = lightIsOff
+            yellowSignal.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenSignal.alpha = lightIsOn
+            yellowSignal.alpha = lightIsOff
+            currentLight = .red
+        }
     }
     
 }
